d0c62367e19b3ffd38e1ebd28bd725927abd816f
kairavkkp <kairavpithadia13@gmail.com>
2020-07-09 19:57:20 +0530

    Assignment 1 Rev 1

--- node-express/index.js
+++ node-express/index.js
@@ -4,6 +4,8 @@ const morgan = require("morgan")
 const bodyParser = require("body-parser")
 
 const dishRouter = require("./routes/dishRouter")
+const promoRouter = require("./routes/promoRouter")
+const leaderRouter = require("./routes/leaderRouter")
 
 const hostname = "localhost"
 const port = 3000
@@ -12,9 +14,15 @@ const app = express()
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 
-
 app.use('/dishes',dishRouter)
 
+app.use('/dishes/:dishId',dishRouter)
+
+app.use('/promotions',promoRouter)
+app.use('/promotions/:promoId',promoRouter)
+
+app.use('/leaders',leaderRouter)
+app.use('leaders/:leaderId',leaderRouter)
 app.use(express.static(__dirname+'/public'))
 
 
