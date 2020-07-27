0ee384e92acb91e252397d2d78322439f28c09ac
Kairav Pithadia <kairavpithadia13@gmail.com>
2020-07-27 12:07:55 +0530

    Assignment 2

--- conFusionServer/routes/leaderRouter.js
+++ conFusionServer/routes/leaderRouter.js
@@ -1,42 +1,58 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const mongoose = require('mongoose')
+const Leaders = require('../models/leaders')
 
 const leaderRouter = express.Router()
 
 leaderRouter.use(bodyParser.json())
 leaderRouter.route('/')
-.all((req,res,next) => {
-    res.statusCode = 200
-    res.setHeader("Content-type","text/plain")
-    next()
-
-})
 .get((req,res,next) => {
-    res.end("Will send all the leaders to you!")
+    Leaders.find({})
+    .then((leader)=>{
+        res.statusCode = 200
+        res.setHeader("Content-Type","application/json")
+        res.json(leader)
+    },(err)=>next(err))
+    .catch((err)=>console.log(err))
 
 })
 .post((req,res,next) => {
 
-    res.end("Will add the leader: "+ req.body.name + " with details: "+req.body.description)
 
+    Leaders.create(req.body)
+    .then((leader)=>{
+        console.log("Leader Created ",leader)
+        res.statusCode = 200
+        res.setHeader("Content-Type","application/json")
+        res.json(leader)
+
+    },(err)=>next(err))
+    .catch((err)=>console.log(err))
 })
 .put((req,res,next) => {
     res.statusCode = 403
     res.end("PUT operation nor supported on /leaders")
 })
 .delete((req,res,next) => {
-    res.end("Deleting all the leaders!")
+    Leaders.deleteMany({})
+    .then((resp)=>{
+        res.statusCode = 200
+        res.setHeader("Content-Type","application/json")
+        res.json(resp)
+    },(err)=>next(err))
+    .catch((err)=>console.log(err))
 });
 
 leaderRouter.route('/:leaderId')
-.all((req,res,next) => {
-    res.statusCode = 200
-    res.setHeader("Content-type","text/plain")
-    next()
-
-})
 .get((req,res,next) => {
-    res.end("Will send details of the leader: "+ req.params.leaderId+" to you!")
+    Leaders.findById(req.params.leaderId)
+    .then((leader)=>{
+        res.statusCode = 200
+        res.setHeader('Content-Type','application/json')
+        res.json(leader)
+    },(err)=>next(err))
+    .catch((err)=>console.log(err))
 
 })
 .post((req,res,next) => {
@@ -45,11 +61,26 @@ leaderRouter.route('/:leaderId')
 
 })
 .put((req,res,next) => {
-    res.write('Updating the leader: '+ req.params.leaderId +"\n")
-    res.end("Will update the leader : "+ req.body.name + " with details: "+req.body.description)
+    Leaders.findByIdAndUpdate(req.params.leaderId,{
+        $set:req.body
+    },{
+        new:true
+    })
+    .then((leader)=>{
+        res.statusCode = 200
+        res.setHeader('Content-Type','application/json')
+        res.json(leader)
+    },(err)=>next(err))
+    .catch((err)=>console.log(err))
 })
 .delete((req,res,next) => {
-    res.end("Deleting leader: "+req.params.leaderId)
+    Leaders.findByIdAndRemove(req.params.leaderId)
+    .then((resp)=>{
+        res.statusCode = 200
+        res.setHeader("Content-Type","application/json")
+        res.json(resp)
+    },(err)=>next(err))
+    .catch((err)=>console.log(err))
 });
 
 
--- conFusionServer/routes/promoRouter.js
+++ conFusionServer/routes/promoRouter.js
@@ -1,23 +1,30 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-
+const mongoose = require('mongoose')
+const Promotions = require('../models/promotions')
 const promoRouter = express.Router()
 
 promoRouter.use(bodyParser.json())
 promoRouter.route('/')
-.all((req,res,next) => {
-    res.statusCode = 200
-    res.setHeader("Content-type","text/plain")
-    next()
-
-})
 .get((req,res,next) => {
-    res.end("Will send all the promotions to you!")
+   Promotions.find({})
+   .then((promotion)=>{
+        res.statusCode = 200
+        res.setHeader("Content-Type","application/json")
+        res.json(promotion)
+   },(err)=>next(err))
+   .catch((err)=>console.log(err))
 
 })
 .post((req,res,next) => {
-
-    res.end("Will add the promotion: "+ req.body.name + " with details: "+req.body.description)
+    Promotions.create(req.body)
+    .then((promotion)=>{
+        console.log("Promotion Created ",promotion)
+        res.statusCode = 200
+        res.setHeader("Content-Type","application/json")
+        res.json(promotion)
+   },(err)=>next(err))
+   .catch((err)=>console.log(err))
 
 })
 .put((req,res,next) => {
@@ -25,18 +32,24 @@ promoRouter.route('/')
     res.end("PUT operation nor supported on /promotions")
 })
 .delete((req,res,next) => {
-    res.end("Deleting all the promotions!")
+    Promotions.deleteMany({})
+    .then((resp)=>{
+        res.statusCode = 200
+        res.setHeader("Content-Type","application/json")
+        res.json(resp)
+   },(err)=>next(err))
+   .catch((err)=>console.log(err))
 });
 
 promoRouter.route('/:promoId')
-.all((req,res,next) => {
-    res.statusCode = 200
-    res.setHeader("Content-type","text/plain")
-    next()
-
-})
 .get((req,res,next) => {
-    res.end("Will send details of the promotion: "+ req.params.promoId+" to you!")
+    Promotions.findById(req.params.promoId)
+    .then((promotion)=>{
+        res.statusCode = 200
+        res.setHeader("Content-Type","application/json")
+        res.json(promotion)
+   },(err)=>next(err))
+   .catch((err)=>console.log(err))
 
 })
 .post((req,res,next) => {
@@ -45,11 +58,26 @@ promoRouter.route('/:promoId')
 
 })
 .put((req,res,next) => {
-    res.write('Updating the promotion: '+ req.params.promoId +"\n")
-    res.end("Will update the promotion : "+ req.body.name + " with details: "+req.body.description)
+    Promotions.findByIdAndUpdate(req.params.promoId,{
+        $set:req.body
+    },{
+        new:true
+    })
+    .then((promotion)=>{
+        res.statusCode = 200
+        res.setHeader("Content-Type","application/json")
+        res.json(promotion)
+   },(err)=>next(err))
+   .catch((err)=>console.log(err))
 })
 .delete((req,res,next) => {
-    res.end("Deleting promotion: "+req.params.promoId)
+    Promotions.findByIdAndRemove(req.params.promoId)
+    .then((resp)=>{
+        res.statusCode = 200
+        res.setHeader("Content-Type","application/json")
+        res.json(resp)
+   },(err)=>next(err))
+   .catch((err)=>console.log(err))
 });
 
 
