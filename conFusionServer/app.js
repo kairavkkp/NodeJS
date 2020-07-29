d8f20e8f676f6c9086f8cf6283807d77dda586ca
Kairav Pithadia <kairavpithadia13@gmail.com>
2020-07-29 13:01:47 +0530

    Express Sessions Part 1

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session')
+var FileStore = require('session-file-store')(session)
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -34,13 +36,20 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+//app.use(cookieParser('12345-67890-09876-54321'));
 
+app.use(session({
+  name:'session-id',
+  secret:'12345-67890-09876-54321',
+  saveUninitialized:false,
+  resave:false,
+  store:new FileStore()
+}));
 
 function auth(req,res,next){
-  console.log(req.signedCookies)
-
-    if(!req.signedCookies.user){
+  
+  console.log(req.session)
+    if(!req.session.user){
       var authHeader = req.headers.authorization;
 
       if(!authHeader){
@@ -58,7 +67,7 @@ function auth(req,res,next){
       var password = auth[1]
 
       if(username === 'admin' && password === "password"){
-        res.cookie('user', 'admin',{signed:true})
+        req.session.user = 'admin'
         next();
 
       }
@@ -72,7 +81,7 @@ function auth(req,res,next){
     }
 
     else{
-      if(req.signedCookies.user === 'admin'){
+      if(req.session.user === 'admin'){
         next();
       }
       else{
