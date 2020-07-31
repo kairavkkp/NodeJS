85a82bf4453466b0a0c956660ede4f0d225f0c64
kairavkkp <kairavpithadia13@gmail.com>
2020-07-31 13:06:07 +0530

    Passport

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -5,6 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session')
 var FileStore = require('session-file-store')(session)
+var passport = require('passport')
+var authenticate = require('./authenticate')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/userRouter');
@@ -45,34 +48,23 @@ app.use(session({
   resave:false,
   store:new FileStore()
 }));
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req,res,next){
-  
-  console.log(req.session)
-    if(!req.session.user){
-      var authHeader = req.headers.authorization;
-
+      if(!req.user){
       var err = new Error('You are not authorized!')
-      res.setHeader('WWW-Authenticate','Basic')
       err.status = 403
       return next(err)
     }
     
     else{
-      if(req.session.user === 'authenticated'){
-        next();
-      }
-      else{
-        var err = new Error('You are not authorized!')
-        err.status = 403
-        return next(err)
-      }
+      next();
     }
-
-
-  
 
 }
 
--- conFusionServer/models/users.js
+++ conFusionServer/models/users.js
@@ -1,20 +1,14 @@
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
+var passportLocalMongoose = require('passport-local-mongoose')
 var userSchema = new Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
     admin:{
         type:Boolean,
         default:false
     }
 })
+
+userSchema.plugin(passportLocalMongoose)
 
 module.exports = mongoose.model('User',userSchema)
